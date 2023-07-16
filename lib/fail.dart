449a806b9ee0aac1df81e657658bd0c0ceff78f0
su-giana <giananews@gmail.com>
2023-07-16 23:12:28 +0900

    UI impelmentation done

--- lib/fail.dart
+++ lib/fail.dart
@@ -8,43 +8,45 @@ class FailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.red, width: 2),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.red, width: 2),
+                ),
+                child: Icon(
+                  Icons.clear,
+                  color: Colors.red,
+                  size: 125,
+                ),
               ),
-              child: Icon(
-                Icons.clear,
-                color: Colors.red,
-                size: 150,
+              SizedBox(height: 30),
+              Text(
+                '결제가 실패하였습니다',
+                style: TextStyle(fontFamily: "mainfont", fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
               ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              '결제가 실패하였습니다',
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black),
-            ),
-            SizedBox(height: 10),
-            Text(
-              '송금자: ${item.senderName} | 입금자: ${item.receiverName}',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
-            ),
-            SizedBox(height: 5),
-            Text(
-              '결제 금액: ${item.trans.cost}',
-              style: TextStyle(fontSize: 16, color: Colors.grey),
-            ),
-            SizedBox(height: 5),
-          ],
+              SizedBox(height: 20),
+              Text(
+                '송금자: ${item.senderName} | 입금자: ${item.receiverName}',
+                style: TextStyle(fontFamily: "mainfont", fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              SizedBox(height: 20),
+              Text(
+                '결제 금액: ${item.trans.cost}',
+                style: TextStyle(fontFamily: "mainfont", fontSize: 20, color: Colors.grey),
+              ),
+              SizedBox(height: 5),
+            ],
+          ),
         ),
       ),
     );
--- lib/success.dart
+++ lib/success.dart
@@ -4,42 +4,51 @@ import 'package:flutter_application_1/my_transaction/my_transaction_screen.dart'
 class SuccessScreen extends StatelessWidget {
   final TransactionWithName item;
 
-  const SuccessScreen({super.key, required this.item});
+  const SuccessScreen({Key? key, required this.item}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          width: 40,
-          height: 40,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(color: Colors.blue, width: 2),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.blue, width: 2),
+                ),
+                child: Icon(
+                  Icons.check,
+                  color: Colors.blue,
+                  size: 125,
+                ),
+              ),
+              SizedBox(height: 30),
+              Text(
+                '결제가 성공하였습니다',
+                style: TextStyle(fontFamily: "mainfont", fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              SizedBox(height: 20),
+              Text(
+                '송금자: ${item.senderName} | 입금자: ${item.receiverName}',
+                style: TextStyle(fontFamily: "mainfont", fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              SizedBox(height: 20),
+              Text(
+                '결제 금액: ${item.trans.cost}',
+                style: TextStyle(fontFamily: "mainfont", fontSize: 20, color: Colors.grey),
+              ),
+              SizedBox(height: 5),
+            ],
           ),
-          child: Icon(
-            Icons.check,
-            color: Colors.blue,
-          ),
         ),
-        SizedBox(height: 10),
-        Text(
-          '결제가 완료되었습니다',
-          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(height: 10),
-        Text(
-          '송금자 : ${item.senderName} | 입금자 : ${item.receiverName}',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(height: 5),
-        Text(
-          '결제 금액 : ${item.trans.cost}',
-          style: TextStyle(fontSize: 16, color: Colors.grey),
-        ),
-        SizedBox(height: 5),
-      ],
+      ),
     );
   }
 }
