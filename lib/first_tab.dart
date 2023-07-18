056dc4b4306632406813b5f079d6ff87be004735
kai._.mjy <kaimjy@kaist.ac.kr>
2023-07-19 05:47:56 +0900

    2023-07-19 Jiyeon add 귀여운 카드? 계좌 배경을 추가합니다..^^ ehehehehf

--- lib/first_tab.dart
+++ lib/first_tab.dart
@@ -21,6 +21,7 @@ class FirstTab extends StatelessWidget {
   const FirstTab({Key? key}) : super(key: key);
 
   Future<List<Account>> getAccountList() async{
+
     final request  = Uri.parse("$baseUrl/account_list");
     final jwtToken = await getJwtToken();
     final headers = <String, String>{
@@ -97,8 +98,11 @@ class FirstTab extends StatelessWidget {
       }
   }
 
-  FutureBuilder<List<Account>> getMyAccountList()
+  FutureBuilder<List<Account>> getMyAccountList(BuildContext context)
   {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
     return FutureBuilder<List<Account>>(
         future: getAccountList(),
         builder: (BuildContext context, AsyncSnapshot<List<Account>> snapshot)
@@ -109,15 +113,17 @@ class FirstTab extends StatelessWidget {
             return Scaffold(
               body: Center(
                 child: Container(
-                  constraints: const BoxConstraints(maxWidth: 450),
+
+                  // constraints: const BoxConstraints(maxWidth: 450),
                   child: ListView.builder(
                     itemCount: accounts.length,
                     itemBuilder: (BuildContext context, int index) {
                       final item = accounts[index];
                       return Container(
-                        height: 133,
-                        margin:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 5.0),
+                        // height: 133,
+                        // height: height * 0.2,
+                        width: width*0.9,
+                        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 5.0),
                         decoration: BoxDecoration(
                             border: Border.all(color: const Color(0xFFE0E0E0)),
                             borderRadius: BorderRadius.circular(8.0)),
@@ -135,28 +141,46 @@ class FirstTab extends StatelessWidget {
                               Row(
                                 crossAxisAlignment: CrossAxisAlignment.start, // Adjust crossAxisAlignment
                                 children: [
-                                  Expanded(
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(0, 0, 15 ,0),
+                                    child: ClipOval(
+                                      child: Image.asset(
+                                        'assets/images/banklogo.png',
+                                        height: width * 0.15,
+                                        width: width * 0.15,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
                                     child: Column(
                                       mainAxisAlignment: MainAxisAlignment.center,
                                       crossAxisAlignment: CrossAxisAlignment.start,
                                       children: [
                                         Text(
+                                          "매드뱅크",
+                                          style: Theme.of(context).textTheme.caption,
+                                        ),
+                                        SizedBox(height: height * 0.005),
+                                        Text(
+                                          "${item.accountNumber.substring(0, 6)}-${item.accountNumber.substring(6, 9)}-${item.accountNumber.substring(9)}",
+                                          style: const TextStyle(
+                                              // fontWeight: FontWeight.bold,
+                                              fontSize: 16
+                                          ),
+                                        ),
+                                        SizedBox(height: height * 0.005),
+                                        Text(
                                           "${item.balance}원",
-                                          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                                          style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18
+                                          ),
                                           maxLines: 2,
                                           overflow: TextOverflow.ellipsis,
                                         ),
-                                        const SizedBox(height: 10),
-                                        Text(
-                                          "자립예탁금",
-                                          style: Theme.of(context).textTheme.caption,
-                                        ),
-                                        const SizedBox(height: 10),
-                                        Text(
-                                          "계좌 번호 : ${item.accountNumber}",
-                                          style: Theme.of(context).textTheme.caption,
-                                        ),
-                                        const SizedBox(height: 8),
+
+                                        SizedBox(height: height * 0.005),
                                         Text(
                                           "${DateTime.now()}에 갱신됨",
                                           style: Theme.of(context).textTheme.caption,
@@ -164,17 +188,7 @@ class FirstTab extends StatelessWidget {
                                       ],
                                     ),
                                   ),
-                                  Padding(
-                                    padding: const EdgeInsets.fromLTRB(0, 15, 15 ,0),
-                                    child: ClipOval(
-                                      child: Image.asset(
-                                        'assets/images/banklogo.png',
-                                        height: 80,
-                                        width: 80,
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  ),
+
                                 ],
                               ),
                             ],
@@ -286,7 +300,7 @@ class FirstTab extends StatelessWidget {
                 ),
 
                 Expanded(
-                  child: getMyAccountList(),
+                  child: getMyAccountList(context),
                 ),
               ],
             );
